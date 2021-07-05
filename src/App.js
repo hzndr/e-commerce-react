96747f9b2bbbe765f85c8111c978760a13e33dc0
unknown <hania.zander@gmail.com>
2021-07-05 11:28:46 +0200

    integrated firebase into application, created sign in page with sign in component and sign in with google

--- src/App.js
+++ src/App.js
@@ -4,18 +4,39 @@ import {Switch, Route} from 'react-router-dom'
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
+import SignInAndSignOutPage from './pages/sign-in-and-sign-out/sign-in-and-sign-out.component'
+import {auth} from '../src/firebase/firebase.utils'
 
 
-function App() {
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      currentUser: null
+    }
+
+  }
+  unsubscribeFromAuth = null;
+
+  componentDidMount(){
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {this.setState({currentUser: user}); 
+    console.log(user)})
+  }
+
+  componentWillUnmount(){
+    this.unsubscribeFromAuth();
+  }
+  render(){
   return (
     <div >
-    <Header/>
+    <Header currentUser={this.state.currentUser}/>
     <Switch>
     <Route exact path="/" component={HomePage} />
     <Route path='/shop' component={ShopPage}/>
+    <Route path='/signin' component={SignInAndSignOutPage}/>
     </Switch>
     </div>
-  );
+  )}
 }
 
 export default App;
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyCRclj-o81jKQy0BV7tZNu0vU7zLm23t-s",
+    authDomain: "e-commerce-react-268a7.firebaseapp.com",
+    projectId: "e-commerce-react-268a7",
+    storageBucket: "e-commerce-react-268a7.appspot.com",
+    messagingSenderId: "1057624588523",
+    appId: "1:1057624588523:web:df7e87332537e9db503eae",
+    measurementId: "G-LQ6MX9DXTK"
+  };
+
+  firebase.initializeApp(config);
+  
+  export const auth = firebase.auth();
+  export const firestore = firebase.firestore();
+
+  //Set up Google authentication utility
+  const provider = new firebase.auth.GoogleAuthProvider();
+  provider.setCustomParameters({ prompt: 'select_account'});
+  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+  export default firebase;
